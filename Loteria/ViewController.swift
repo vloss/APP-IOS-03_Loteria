fb9b8ce78ef847527bcfa97bb05952de45e54c26
vloss <viniciusvianaloss@gmail.com>
2023-01-06 00:23:39 -0300

    Initial commit

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Vinicius Loss on 06/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

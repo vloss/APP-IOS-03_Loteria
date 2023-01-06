2d3955e32888a9811c68e5361b35d8a26ad35518
vloss <viniciusvianaloss@gmail.com>
2023-01-06 12:35:38 -0300

    Continuação de layout do app.

--- Loteria/ViewController.swift
+++ Loteria/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbGameType: UILabel!
+    
+    @IBOutlet weak var scGameType: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func generateGame() {
+    }
+    
 }
 
